bd41771457ce0e7a39b856e70639d60cb142a22d
Charlotte.Hong <hunandy14@gmail.com>
2022-01-16 16:30:04 +0900

    AutomaticUpdates

---
+++ AutomaticUpdates/AutomaticUpdates.ps1
@@ -0,0 +1,13 @@
+function AutomaticUpdates {
+    param(
+        [switch] $Recovery
+    )
+    # 恢復為未設定狀態
+    if ($Recovery) { reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU; return }
+    # 將自動更新設置為手動
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 00000000 /f
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 00000002 /f
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallDay /t REG_DWORD /d 00000000 /f
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallTime /t REG_DWORD /d 00000003 /f
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallEveryWeek /t REG_DWORD /d 00000001 /f
+}
