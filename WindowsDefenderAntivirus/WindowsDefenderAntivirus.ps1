4a62e4915a96d2c67b60fe1da09f9417540fe460
Charlotte.Hong <hunandy14@gmail.com>
2022-01-16 18:38:44 +0900

    翻車 不知道為什麼 ParameterSetName 不能這樣設定，算了拿掉不重要。

--- WindowsDefenderAntivirus/WindowsDefenderAntivirus.ps1
+++ WindowsDefenderAntivirus/WindowsDefenderAntivirus.ps1
@@ -1,15 +1,14 @@
 function WindowsDefenderAntivirus {
     param(
-        [Parameter(ParameterSetName = "Disable")]
+        [Parameter(ParameterSetName = "")]
         [switch] $Disable,
-        [Parameter(ParameterSetName = "DisableRealtime")]
+        [Parameter(ParameterSetName = "")]
         [switch] $DisableRealtime
     )
-    
     if ($Disable) { # 關閉所有功能
-        reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+        reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /f /v DisableAntiSpyware /t REG_DWORD /d 1
     } elseif ($DisableRealtime) { # 僅關閉即時掃描
-        # reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+        reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /f /v DisableRealtimeMonitoring /t REG_DWORD /d 1
         Write-Host "已關閉即時掃描，但新版 Windows 需要你手動關閉防串改保護才會生效。"  -ForegroundColor:Yellow
         Write-Host "  設定 -> 更新與安全性 -> 開啟Windows安全性(按鈕)" -ForegroundColor:Yellow
         Write-Host "     -> 病毒與威脅防護 -> 病毒與威脅防護設定::管理設定 -> 防串改保護::關閉此項目" -ForegroundColor:Yellow
@@ -17,4 +16,4 @@ function WindowsDefenderAntivirus {
         reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /f /v DisableAntiSpyware
         reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /f /v DisableRealtimeMonitoring
     }
-} #WindowsDefenderAntivirus -DisableRealtime
+} # WindowsDefenderAntivirus -DisableRealtime
