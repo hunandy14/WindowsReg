1a7cf7af7f442196cf0695676f9ee2edc773232a
Charlotte.Hong <hunandy14@gmail.com>
2022-01-19 02:09:51 +0900

    update 更新安裝到開始

--- EdgeMediaControls/EdgeMediaControls.ps1
+++ EdgeMediaControls/EdgeMediaControls.ps1
@@ -6,22 +6,22 @@ function EdgeMediaControls {
         [switch]$Start,
         [switch]$StartUp
     )
-    if ($Desktop) {
-        $Path = [Environment]::GetFolderPath("Desktop")
-    } elseif ($Start) {
-        $Path = "$env:AppData\Microsoft\Windows\Start Menu\Programs"
-    } elseif ($StartUp) {
-        $Path = "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup"
-    }
     # 重啟到 MediaControls 模式
     $edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
     $edgeArgs = "--enable-features=GlobalMediaControls"
     Get-Process|Where-Object{$_.ProcessName.Contains("msedge")} | Stop-Process
     Start-Process $edgePath -ArgumentList:$edgeArgs
     # 安裝到指定位置
-    if ($Path) {
-        $shortcutName = "Edge Media Controls.lnk"
-        $Link = "https://github.com/hunandy14/WindowsReg/raw/master/EdgeMediaControls/Edge%20Media%20Controls.lnk"
+    $shortcutName = "Edge Media Controls.lnk"
+    $Link = "https://github.com/hunandy14/WindowsReg/raw/master/EdgeMediaControls/Edge%20Media%20Controls.lnk"
+    if ($Desktop) {
+        $Path = [Environment]::GetFolderPath("Desktop")
+        Start-BitsTransfer $Link "$Path\$shortcutName"
+    } if ($Start) {
+        $Path = "$env:AppData\Microsoft\Windows\Start Menu\Programs"
+        Start-BitsTransfer $Link "$Path\$shortcutName"
+    } if ($StartUp) {
+        $Path = "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup"
         Start-BitsTransfer $Link "$Path\$shortcutName"
     }
 }
