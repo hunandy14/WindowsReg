69c33c09f94bdd3b42aecc3b5ee11c597f477b30
Charlotte.Hong <hunandy14@gmail.com>
2022-01-19 01:29:55 +0900

    update 更新完成

--- EdgeMediaControls/EdgeMediaControls.ps1
+++ EdgeMediaControls/EdgeMediaControls.ps1
@@ -5,28 +5,20 @@ function EdgeMediaControls {
         [switch]$Desktop,
         [switch]$Start
     )
-    $userDsk = [Environment]::GetFolderPath("Desktop")
-    
     if ($Desktop) {
-        
+        $Path = [Environment]::GetFolderPath("Desktop")
     } elseif ($Start) {
-        
+        $Path = "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup"
     }
     # 重啟到 MediaControls 模式
+    $edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+    $edgeArgs = "--enable-features=GlobalMediaControls"
     Get-Process|Where-Object{$_.ProcessName.Contains("msedge")} | Stop-Process
     Start-Process $edgePath -ArgumentList:$edgeArgs
-    
-    Start-BitsTransfer https://download.sysinternals.com/files/regjump.zip "$userDsk\Edge Media Controls"
+    # 安裝到指定位置
+    if ($Path) {
+        $shortcutName = "Edge Media Controls.lnk"
+        $Link = "https://github.com/hunandy14/WindowsReg/raw/master/EdgeMediaControls/Edge%20Media%20Controls.lnk"
+        Start-BitsTransfer $Link $shortcutName $Path
+    }
 }
-
-# $shortcutName = "Edge_MediaControls"
-# $edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-# $edgeArgs = "--enable-features=GlobalMediaControls"
-
-# $shell = New-Object -ComObject WScript.Shell
-# $userDsk = [Environment]::GetFolderPath("Desktop")
-
-# $shortcut = $shell.CreateShortcut("$userDsk\$shortcutName`.lnk")
-# $shortcut.TargetPath = $edgePath
-# $shortcut.Arguments = $edgeArgs
-# $shortcut.Save()
