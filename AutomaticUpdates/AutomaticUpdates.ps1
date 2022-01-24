4a584ca0de7818243739ab2fa949c20d81221f21
Charlotte.Hong <hunandy14@gmail.com>
2022-01-24 22:19:41 +0900

    更正服務啟動邏輯

--- AutomaticUpdates/AutomaticUpdates.ps1
+++ AutomaticUpdates/AutomaticUpdates.ps1
@@ -1,7 +1,8 @@
 function AutomaticUpdates {
     param(
         [switch] $Manual,
-        [switch] $Stop
+        [switch] $Stop,
+        [switch] $ServiceInfo
     )
     if ($Manual) { # 將自動更新設置為手動
         AutomaticUpdates
@@ -11,15 +12,23 @@ function AutomaticUpdates {
         reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallTime /t REG_DWORD /d 00000003 /f
         reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallEveryWeek /t REG_DWORD /d 00000001 /f
     } elseif ($Stop) {
-        AutomaticUpdates
-        # reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 00000001 /f
-        net stop wuauserv; sleep 1; (Get-Service -Name:wuauserv)|Set-Service -Status:Stopped -StartupType:disabled
-    }
-    else { # 恢復為未設定狀態
-        (Get-Service -Name:wuauserv)|Set-Service -Status:Running -StartupType:Automatic
-        net start wuauserv
         if (Test-Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate") {
             reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f
         }
+        # reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 00000001 /f
+        if ((Get-Service -Name:wuauserv).Status -eq "Running") { net stop wuauserv }
+        (Get-Service -Name:wuauserv)|Set-Service -StartupType:disabled
+        (Get-Service -Name:wuauserv)|Select-Object Name,DisplayName,Status,StartType
+    }
+    else { # 恢復為未設定狀態
+        if (Test-Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate") {
+            reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f
+        }
+        (Get-Service -Name:wuauserv)|Set-Service -StartupType:Automatic
+        if ((Get-Service -Name:wuauserv).Status -eq "Stopped") { net start wuauserv }
+    }
+    # 查看目前服務的狀態
+    if ($ServiceInfo) {
+        (Get-Service -Name:wuauserv)|Select-Object Name,DisplayName,Status,StartType
     }
 }
