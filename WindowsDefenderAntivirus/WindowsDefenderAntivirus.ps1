4e3591568759dc4d77a36e21254fe93b15a94c44
Charlotte.Hong <hunandy14@gmail.com>
2022-01-24 20:59:39 +0900

    新增說明

--- WindowsDefenderAntivirus/WindowsDefenderAntivirus.ps1
+++ WindowsDefenderAntivirus/WindowsDefenderAntivirus.ps1
@@ -8,11 +8,12 @@ function WindowsDefenderAntivirus {
     if ($Disable) { # 關閉所有功能
         WindowsDefenderAntivirus
         reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /f /v DisableAntiSpyware /t REG_DWORD /d 1
+        Write-Host "已關閉防毒軟體，但可能需要重新啟動才會生效。" -ForegroundColor:Yellow
     } elseif ($DisableRealtime) { # 僅關閉即時掃描
         WindowsDefenderAntivirus
         reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /f /v DisableRealtimeMonitoring /t REG_DWORD /d 1
         Write-Host "已關閉即時掃描，但新版 Windows 需要你手動關閉防竄改保護才會生效。" -ForegroundColor:Yellow
-        Write-Host "  設定 -> 更新與安全性 -> 開啟Windows安全性(按鈕)" -ForegroundColor:Yellow
+        Write-Host "  右下角系統圖示::Windows安全性" -ForegroundColor:Yellow
         Write-Host "     -> 病毒與威脅防護 -> 病毒與威脅防護設定::管理設定 -> 防竄改保護::關閉此項目" -ForegroundColor:Yellow
     } else { # 恢復為未設定狀態
         if (Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -ErrorAction SilentlyContinue) {
