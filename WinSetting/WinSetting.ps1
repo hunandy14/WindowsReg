4ea82a1e796b0fc709270b1a91dffedaa244420b
Charlotte.Hong <hunandy14@gmail.com>
2022-01-24 17:20:23 +0900

    WinSetting 修復

--- WinSetting/WinSetting.ps1
+++ WinSetting/WinSetting.ps1
@@ -8,7 +8,7 @@ function WinSetting {
 # Win11右鍵選單自動展開
 function OnceRightClick {
     param (
-        [string] $Once
+        [switch] $Once
     )
     if ($Once) {
         reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /ve /t REG_SZ /f
