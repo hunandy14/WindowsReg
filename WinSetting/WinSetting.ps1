dc8c394a5d5f796a9dd3d93bcd256a94735a7e74
Charlotte.Hong <hunandy14@gmail.com>
2022-02-09 17:51:10 +0900

    更正鍵盤燈號的數值

--- WinSetting/WinSetting.ps1
+++ WinSetting/WinSetting.ps1
@@ -49,7 +49,7 @@ function Setting_User {
     Stop-Process -ProcessName explorer
 
     # NumLock
-    reg add "HKEY_USERS\.DEFAULT\Control Panel\Keyboard" /v InitialKeyboardIndicators /t REG_SZ /d 2 /f
+    reg add "HKEY_USERS\.DEFAULT\Control Panel\Keyboard" /v InitialKeyboardIndicators /t REG_SZ /d 2147483650 /f
     
     # 新注音預設為英文狀態
     reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\IME\15.0\IMETC" /v "Default Input Mode" /t REG_SZ /d "0x00000001" /f
