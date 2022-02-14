c3d40fd2a1b2db7c83724fcfb012cc51319bce29
Charlotte.Hong <hunandy14@gmail.com>
2022-02-14 14:49:50 +0900

    火狐停用更新 增加權限提示

--- Software/FirefoxUpdate/FirefoxUpdate.ps1
+++ Software/FirefoxUpdate/FirefoxUpdate.ps1
@@ -7,6 +7,9 @@ function FirefoxUpdate {
     $FirfoxPath = "C:\Program Files\Mozilla Firefox"
     $File = "$FirfoxPath\distribution\$FileName"
     
+    Try { [io.file]::OpenWrite("$FirfoxPath\update-settings.ini").close() }
+    catch { Write-Warning "[權限不足]::需要管理員權限或開放火狐資料夾存取權限給當前使用者。"; return }
+    
     if ($Dislable) {
         $Config = "`{
     `"policies`": `{
@@ -14,7 +17,7 @@ function FirefoxUpdate {
     `}
 `}"
         if ( Test-Path $File ) { Move-Item $File "$File.backup2" -Force }
-        (New-Item $File -ItemType:File -Force)|Out-Null
+        New-Item $File -ItemType:File -Force -ErrorAction:Stop
         [System.IO.File]::WriteAllText($File, $Config)
         Write-Host "追加設定檔：" -NoNewline
         Write-Host $File -ForegroundColor:Yellow
@@ -24,14 +27,12 @@ function FirefoxUpdate {
         
     } elseif ($Enable) {
         if ( Test-Path $File ) {
-            Move-Item $File "$File.backup" -Force
+            Move-Item $File "$File.backup" -Force -ErrorAction:Stop 
             Get-Process|Where-Object{$_.ProcessName.Contains("firefox")} | Stop-Process
             Start-Process "C:\Program Files\Mozilla Firefox\firefox.exe"
         }
         Write-Host "已恢復火狐更新"
     }
-    
-
 } 
 # FirefoxUpdate -Enable
 # FirefoxUpdate -Dislable
