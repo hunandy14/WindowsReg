6fbf86e29b5b11e83ddd49dbbc3bc5cce2eb3eeb
Charlotte.Hong <hunandy14@gmail.com>
2022-02-14 14:12:51 +0900

    火狐停用更新 修復重複執行的問題

--- Software/FirefoxUpdate/FirefoxUpdate.ps1
+++ Software/FirefoxUpdate/FirefoxUpdate.ps1
@@ -13,23 +13,25 @@ function FirefoxUpdate {
         `"DisableAppUpdate`": true
     `}
 `}"
-        if ( Test-Path $File ) {
-            [System.IO.File]::WriteAllText($File, $Config)
-        } else {
-            Write-Host "追加設定檔：" "$FirfoxPath\$FileName"
-            (New-Item $File -ItemType:File -Force)|Out-Null
-            [System.IO.File]::WriteAllText($File, $Config)
-            Write-Host "已停用火狐更新"
-        }
+        if ( Test-Path $File ) { Move-Item $File "$File.backup2" -Force }
+        (New-Item $File -ItemType:File -Force)|Out-Null
+        [System.IO.File]::WriteAllText($File, $Config)
+        Write-Host "追加設定檔：" -NoNewline
+        Write-Host $File -ForegroundColor:Yellow
+        Get-Process|Where-Object{$_.ProcessName.Contains("firefox")} | Stop-Process
+        Start-Process "C:\Program Files\Mozilla Firefox\firefox.exe"
+        Write-Host "已停用火狐更新"
+        
     } elseif ($Enable) {
         if ( Test-Path $File ) {
             Move-Item $File "$File.backup" -Force
+            Get-Process|Where-Object{$_.ProcessName.Contains("firefox")} | Stop-Process
+            Start-Process "C:\Program Files\Mozilla Firefox\firefox.exe"
         }
         Write-Host "已恢復火狐更新"
     }
     
-    Get-Process|Where-Object{$_.ProcessName.Contains("firefox")} | Stop-Process
-    Start-Process "C:\Program Files\Mozilla Firefox\firefox.exe"
+
 } 
 # FirefoxUpdate -Enable
 # FirefoxUpdate -Dislable
