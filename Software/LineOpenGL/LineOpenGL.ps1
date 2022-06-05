b480f05929eb2e811005f486d3ed230732f985ca
Charlotte.Hong <hunandy14@gmail.com>
2022-06-06 01:05:49 +0900

    電腦版Line打不開圖片

---
+++ Software/LineOpenGL/LineOpenGL.ps1
@@ -0,0 +1,27 @@
+
+function OpenGLDetected {
+    param (
+        [Parameter(Position = 0, ParameterSetName = "")]
+        [switch] $Enbale,
+        [Parameter(Position = 0, ParameterSetName = "")]
+        [switch] $Disbale
+    )
+    $SetttingPath = $([Environment]::GetFolderPath('LocalApplicationData')) + "\LINE\Data\plugin\LineMediaPlayer\setting.ini"
+    $Content = (Get-Content $SetttingPath)
+
+    if ($Enbale) {
+        if ( $Content.IndexOf("SupportedOpenGLDetected=false") -ne -1 ) {
+            (Invoke-RestMethod 'raw.githubusercontent.com/hunandy14/cvEncode/master/cvEncoding.ps1') | Invoke-Expression;
+            $Content = $Content.Replace("SupportedOpenGLDetected=false", "SupportedOpenGLDetected=true")
+            $Content | WriteContent $SetttingPath
+        } Write-Host "啟用 OpenGL 完成"
+    } elseif ($Disbale) {
+        if ( $Content.IndexOf("SupportedOpenGLDetected=true") -ne -1 ) {
+            (Invoke-RestMethod 'raw.githubusercontent.com/hunandy14/cvEncode/master/cvEncoding.ps1') | Invoke-Expression;
+            $Content = $Content.Replace("SupportedOpenGLDetected=true", "SupportedOpenGLDetected=false")
+            $Content | WriteContent $SetttingPath
+        } Write-Host "禁用 OpenGL 完成"
+    } else {
+        Write-Host "當前狀態："$Content[1]
+    }
+} OpenGLDetected
