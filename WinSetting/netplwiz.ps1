f57ef8d2f59ab8a918eace6245aa5b6b9a767783
Charlotte.Hong <hunandy14@gmail.com>
2022-06-09 00:11:46 +0900

    update

--- WinSetting/netplwiz.ps1
+++ WinSetting/netplwiz.ps1
@@ -5,6 +5,6 @@ function netplwiz {
     $regPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device"
     $regItem = "DevicePasswordLessBuildVersion"
     $regValue = 0
-    reg add $regPath /v $regItem /t REG_DWORD /d $regValue /f
+    reg add $regPath /v $regItem /t REG_DWORD /d $regValue /f | Out-Null
     if (!$OnlyReg) { Netplwiz.exe }
 } # netplwiz
