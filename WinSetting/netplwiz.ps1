7fdc45ca971ecdbc74234a192c961de4f2efae5d
Charlotte.Hong <hunandy14@gmail.com>
2022-06-09 00:05:01 +0900

    獨立拉出netlpwiz函式

---
+++ WinSetting/netplwiz.ps1
@@ -0,0 +1,10 @@
+function netplwiz {
+    param (
+        [switch] $OnlyReg
+    )
+    $regPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device"
+    $regItem = "DevicePasswordLessBuildVersion"
+    $regValue = 0
+    reg add $regPath /v $regItem /t REG_DWORD /d $regValue /f
+    if (!$OnlyReg) { Netplwiz.exe }
+} # netplwiz
