01df3ac80766f460600026501e9408b9adf63585
Charlotte.Hong <hunandy14@gmail.com>
2022-06-09 01:58:15 +0900

    更新 NumLock 無效的問題

---
+++ WinSetting/InitialKeyboard.ps1
@@ -0,0 +1,15 @@
+###################################################################################################
+# InitialKeyboardIndicators 數值含義：
+    # 0: 關閉全部鎖定
+    # 1: 單獨開啟大寫鎖定Caps Lock
+    # 2: 單獨開啟數字鎖定Num Lock
+    # 3: 開啟大寫和數字鎖定
+    # 4: 單獨開啟滾動鎖定ScrollLock
+    # 5: 開啟大寫和滾動鎖定
+    # 6: 開啟數字和滾動鎖定
+    # 7: 開啟全部鎖定
+###################################################################################################
+function InitialKeyboard {
+    $UserSID  = (Get-LocalUser -Name:$env:USERNAME).sid.value
+    reg add "HKEY_USERS\$UserSID\Control Panel\Keyboard" /v "DevicePasswordLessBuildVersion" /t REG_SZ /d 2 /f
+} # InitialKeyboard
