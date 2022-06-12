10fbc02aa39cac4e83a10163904677e39d4167dc
Charlotte.Hong <hunandy14@gmail.com>
2022-06-13 01:22:12 +0900

    店員設置:: 關閉CPU自動超頻

---
+++ PowercfgPerfBoost/PowercfgPerfBoost.ps1
@@ -0,0 +1,62 @@
+# 預設電源方案
+function __LoadSetting__ {
+    $Powercfg_PowerSaver      = 'a1841308-3541-4fab-bc81-f71556f20b4a'
+    $Powercfg_Balanced        = '381b4222-f694-41f0-9685-ff5bb260df2e'
+    $Powercfg_HighPerformance = '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c'
+    $Powercfg_Current         = "scheme_current"
+} __LoadSetting__
+function GET-PowercfgScheme {
+    powercfg.exe /l
+}
+
+function CopyScheme {
+    param (
+        [Parameter(Position = 0, ParameterSetName = "", Mandatory)]
+        [string] $Name,
+        [string] $GUID,
+        [switch] $Apply
+    )
+    # 未輸入已當前為主
+    if (!$GUID) { $GUID = $Powercfg_Current}
+    # 複製方案
+    $newScheme = powercfg /duplicatescheme $GUID
+    $GUID = $newScheme.Substring($newScheme.IndexOf(' GUID: ') + 7, 36)
+    powercfg /changename $GUID $Name
+    # 套用方案
+    if ($Apply) { Powercfg -setactive $GUID }
+    return $GUID
+} # CopyScheme "電池保護" -Apply
+
+function Set-PerfBoost {
+    param (
+        [string] $Value,
+        [string] $GUID,
+        [switch] $Apply
+    )
+    if (!$Value) {
+        Write-Host ''
+        Write-Host '[錯誤]' -ForegroundColor:DarkRed -NoNewline
+        Write-Host '::未輸入Value數值，使用方法請參考下列說明'
+        Write-Host ''
+        Write-Host '使用範例：'
+        Write-Host '  Set-PerfBoost -Value:0' -ForegroundColor:Yellow
+        Write-Host ''
+        Write-Host 'Value: 數值意義'
+        Write-Host '  0: 已停用'
+        Write-Host '  1: 啟用'
+        Write-Host '  2: 主動'
+        Write-Host '  3: 已啟用有效率'
+        Write-Host '  4: 有效率地積極'
+        Write-Host ''
+    } else {
+        if (!$GUID) { $GUID = $Powercfg_Current }
+        (reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\be337238-0d82-4146-a960-4f3749d470c7" /v Attributes /t REG_DWORD /d 2 /f) | Out-Null
+        Powercfg -setacvalueindex $GUID sub_processor PERFBOOSTMODE $Value
+        Powercfg -setdcvalueindex $GUID sub_processor PERFBOOSTMODE $Value
+        if ($Apply) { Powercfg -setactive $GUID }
+    }
+}
+
+function PowercfgPerfBoost {
+    Set-PerfBoost -Value:0 -GUID:(CopyScheme "電池保護") -Apply
+} # PowercfgPerfBoost
