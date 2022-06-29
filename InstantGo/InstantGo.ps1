0bcc5ba7e88beeddafa2c688894729bc6074eb29
Charlotte.Hong <hunandy14@gmail.com>
2022-06-29 22:26:32 +0900

    更新無法重新開啟的錯誤

--- InstantGo/InstantGo.ps1
+++ InstantGo/InstantGo.ps1
@@ -1,29 +1,52 @@
+function DisableInstantGo {
+    param (
+        [Parameter(Position = 0, ParameterSetName = "Recovery")]
+        [switch] $Recovery
+    )
+    # 檢查
+    $IsLaptop = ((Get-WmiObject -Class Win32_ComputerSystem -Property PCSystemType).PCSystemType) -eq 2
+    if (!$IsLaptop) { Write-Host "Error:: This PC is not Laptop." return }
+    
+    # 主功能
+    $VersionFlag = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion[0]
+    if ($VersionFlag -ne '1') {
+        # Windwos 20H1 以後
+        if (!$Recovery) {
+            reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v 'PlatformAoAcOverride' /t REG_DWORD /d 0 /f
+        } else {
+            if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power" -Name:'PlatformAoAcOverride' -EA:0) {
+                reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v 'PlatformAoAcOverride' /f
+            }
+            if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power" -Name:'CsEnabled' -EA:0) {
+                reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v 'CsEnabled' /f
+            }
+        }
+    } else { 
+        # Windwos 1903 以前
+        if (!$Recovery) {
+            reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v 'CsEnabled' /t REG_DWORD /d 0 /f
+        } else {
+            reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v 'CsEnabled' /t REG_DWORD /d 1 /f
+        }
+    }
+}
+
 function InstantGo {
     param (
-        [Parameter(Position = 0, ParameterSetName = "Disable", Mandatory=$true)]
+        [Parameter(Position = 0, ParameterSetName = "Disable", Mandatory)]
         [switch] $Disable,
-        [Parameter(Position = 0, ParameterSetName = "Enable", Mandatory=$true)]
+        [Parameter(Position = 0, ParameterSetName = "Enable", Mandatory)]
         [switch] $Enable,
-        [Parameter(Position = 0, ParameterSetName = "Info", Mandatory=$true)]
+        [Parameter(Position = 0, ParameterSetName = "Info", Mandatory)]
         [switch] $Info
     )
     if ($Disable) {
-        if (Get-ItemProperty -Path "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" -Name CsEnabled -ErrorAction SilentlyContinue) {
-            reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v CsEnabled /t REG_DWORD /d 0 /f
-        } else {
-            reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v PlatformAoAcOverride /t REG_DWORD /d 0 /f
-        }
-        Write-Host "執行完畢，重新啟動後生效" -ForegroundColor:Yellow
+        DisableInstantGo
     } elseif ($Enable) {
-        if (Get-ItemProperty -Path "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" -Name CsEnabled -ErrorAction SilentlyContinue) {
-            reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v CsEnabled /t REG_DWORD /d 1 /f
-        } else {
-            if (Get-ItemProperty -Path "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" -Name PlatformAoAcOverride -ErrorAction SilentlyContinue) {
-                reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power" /v PlatformAoAcOverride /f
-            }
-        }
-        Write-Host "執行完畢，重新啟動後生效" -ForegroundColor:Yellow        
+        DisableInstantGo -Recovery
     } elseif ($Info) {
-        powercfg -a
+        Powercfg.exe -a
     }
-} # InstantGo -Info
+}
+# InstantGo -Disable
+# InstantGo -Enable
