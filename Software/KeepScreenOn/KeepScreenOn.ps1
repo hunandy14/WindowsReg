2b12525b3750bd603ef62dedbca958adaee27015
Charlotte.Hong <hunandy14@gmail.com>
2022-10-14 14:51:20 +0900

    KeepScreenOn

---
+++ Software/KeepScreenOn/KeepScreenOn.ps1
@@ -0,0 +1,19 @@
+# 保持螢幕亮著
+function KeepScrOn ([UInt64] $Time=59) {
+    # https://gist.github.com/jamesfreeman959/231b068c3d1ed6557675f21c0e346a9c
+    $Msg = "Running KeepScrOn... (Press Ctrl+C to end.)"
+    Write-Host "[$((Get-Date).Tostring("yyyy/MM/dd HH:mm:ss.fff"))] $Msg"
+    
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    while (1) {
+        $Pos = [System.Windows.Forms.Cursor]::Position
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X+1), ($Pos.Y+0))
+        if($Pos -eq [System.Windows.Forms.Cursor]::Position){
+            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X-1), ($Pos.Y+0))
+            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X+1), ($Pos.Y+0))
+        } else {
+            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X-1), ($Pos.Y+0))
+        }
+        Start-Sleep -Seconds $Time
+    }
+} # KeepScrOn
---
+++ Software/KeepScreenOn/README.md
@@ -0,0 +1,12 @@
+保持螢幕亮著
+===
+
+快速使用
+```ps1
+irm bit.ly/KeepScrOn|iex; KeepScrOn
+```
+
+捷徑使用(新增捷徑之後這行打進去)
+```ps1
+powershell -NoExit -C "&{irm bit.ly/KeepScrOn|iex; KeepScrOn}"
+```
