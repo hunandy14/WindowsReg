6cb943f664074adb91e962ee93f0c3ead66af1d4
Charlotte.Hong <hunandy14@gmail.com>
2022-10-14 16:20:49 +0900

    修正移動越來越左的問題，新增debug模式

--- Software/KeepScreenOn/KeepScreenOn.ps1
+++ Software/KeepScreenOn/KeepScreenOn.ps1
@@ -1,19 +1,17 @@
 # 保持螢幕亮著
-function KeepScrOn ([UInt64] $Time=59) {
+function KeepScrOn ([UInt64] $Time=59, $Offset = 1, [Switch] $Debug) {
     # https://gist.github.com/jamesfreeman959/231b068c3d1ed6557675f21c0e346a9c
     $Msg = "Running KeepScrOn... (Press Ctrl+C to end.)"
     Write-Host "[$((Get-Date).Tostring("yyyy/MM/dd HH:mm:ss.fff"))] $Msg"
-    
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     while (1) {
         $Pos = [System.Windows.Forms.Cursor]::Position
-        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)+1), $Pos.Y)
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)+$offset), $Pos.Y)
         if($Pos -eq [System.Windows.Forms.Cursor]::Position){
-            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)-1), $Pos.Y)
-            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)+1), $Pos.Y)
-        } else {
-            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)-1), $Pos.Y)
+            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)-$offset), $Pos.Y)
         }
+        if ($Debug) { Start-Sleep -Seconds 0.01 }
+        [System.Windows.Forms.Cursor]::Position = $Pos
         Start-Sleep -Seconds $Time
     }
-} # KeepScrOn
+} # KeepScrOn -Time 3 -Offset 100 -Debug
