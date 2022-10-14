ad8202d74e8419e486fb8d81394e48e24870495e
Charlotte.Hong <hunandy14@gmail.com>
2022-10-14 15:52:28 +0900

    更正沒有移動的bug (應該是沒括號被當作字串處理了)

--- Software/KeepScreenOn/KeepScreenOn.ps1
+++ Software/KeepScreenOn/KeepScreenOn.ps1
@@ -7,12 +7,12 @@ function KeepScrOn ([UInt64] $Time=59) {
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     while (1) {
         $Pos = [System.Windows.Forms.Cursor]::Position
-        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X+1), ($Pos.Y+0))
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)+1), $Pos.Y)
         if($Pos -eq [System.Windows.Forms.Cursor]::Position){
-            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X-1), ($Pos.Y+0))
-            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X+1), ($Pos.Y+0))
+            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)-1), $Pos.Y)
+            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)+1), $Pos.Y)
         } else {
-            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(($Pos.X-1), ($Pos.Y+0))
+            [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)-1), $Pos.Y)
         }
         Start-Sleep -Seconds $Time
     }
