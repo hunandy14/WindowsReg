fb9125ddf9a32c2bd47421d159ba4e8e57e5c50f
Charlotte.Hong <hunandy14@gmail.com>
2022-10-14 16:38:49 +0900

    更新debug延遲到1S，螢幕太爛居然會看不出來...

--- Software/KeepScreenOn/KeepScreenOn.ps1
+++ Software/KeepScreenOn/KeepScreenOn.ps1
@@ -1,6 +1,11 @@
-# 保持螢幕亮著
-function KeepScrOn ([UInt64] $Time=59, $Offset = 1, [Switch] $Debug) {
-    # https://gist.github.com/jamesfreeman959/231b068c3d1ed6557675f21c0e346a9c
+# 保持螢幕亮著 (https://gist.github.com/jamesfreeman959/231b068c3d1ed6557675f21c0e346a9c)
+function KeepScrOn {
+    Param(
+        [UInt64] $Time=59, 
+        [UInt64] $Offset=1, 
+        [Switch] $Debug
+    )
+    if ($Debug) {$Offset=100}
     $Msg = "Running KeepScrOn... (Press Ctrl+C to end.)"
     Write-Host "[$((Get-Date).Tostring("yyyy/MM/dd HH:mm:ss.fff"))] $Msg"
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
@@ -10,8 +15,8 @@ function KeepScrOn ([UInt64] $Time=59, $Offset = 1, [Switch] $Debug) {
         if($Pos -eq [System.Windows.Forms.Cursor]::Position){
             [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X)-$Offset), $Pos.Y)
         }
-        if ($Debug) { Start-Sleep -Seconds 0.1 }
+        if ($Debug) { Start-Sleep -Seconds 1 }
         [System.Windows.Forms.Cursor]::Position = $Pos
         Start-Sleep -Seconds $Time
     }
-} # KeepScrOn -Time 3 -Offset 100 -Debug
+} # KeepScrOn 1 -Debug
