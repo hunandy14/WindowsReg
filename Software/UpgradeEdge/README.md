0abd3081cf2c7b12e33aa1edc17029b886d2884d
Charlotte.Hong <hunandy14@gmail.com>
2022-11-22 01:43:07 +0900

    自動更新 Edge

---
+++ Software/UpgradeEdge/README.md
@@ -0,0 +1,7 @@
+更新Edge
+===
+
+快速使用
+```ps1
+irm bit.ly/UpgradeEdge|iex
+```
---
+++ Software/UpgradeEdge/UpgradeEdge.ps1
@@ -0,0 +1,21 @@
+function AutoUpgradeEdge([Switch] $StopEdgeProcess) {
+    # 獲取當前版本資訊
+    $EdgePath = (Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe' "(default)")
+    $CurrVer  = (Get-Item $EdgePath).VersionInfo.ProductVersion
+    # 獲取最新版本資訊
+    $Info     = (Invoke-RestMethod "https://edgeupdates.microsoft.com/api/products")[0].Releases[2]
+    $NewVer   = $Info.ProductVersion
+    $Url      = $Info.Artifacts.Location; $Url -match "[^/]+(?!.*/)" |Out-Null
+    $FileName = $Matches[0]
+    # 檢查版本
+    if ($CurrVer -eq $NewVer) {
+        Write-Host "Edge Already the latest version $CurrVer" -ForegroundColor:Yellow
+    } else {
+        $DLPath = "$env:TEMP\$NewVer\$FileName"
+        if (!(Test-Path $DLPath)) { New-Item $DLPath -ItemType:File -Force|Out-Null } 
+        Start-BitsTransfer $Url $DLPath
+        Start-Process msiexec.exe -ArgumentList "/i $DLPath /norestart" -Wait
+        Write-Host "Edge has been updated to the latest version $NewVer" -ForegroundColor:Yellow
+        if($StopEdgeProcess) {Stop-Process -ProcessName:'msedge'}
+    }
+} AutoUpgradeEdge
