b44688d1236638bde52c2709d73b4baea61494be
Charlotte.Hong <hunandy14@gmail.com>
2022-12-19 02:15:33 +0900

    RegUnityly:: Update

--- RegUnitily/RegUnitily.ps1
+++ RegUnitily/RegUnitily.ps1
@@ -1,11 +1,14 @@
 function RegAdd {
     param (
+        # 1. 路徑
         [Parameter(Position = 0, ParameterSetName = "", Mandatory)]
         [String] $Path,
+        # 2. 名稱
         [Parameter(Position = 1, ParameterSetName = "A", Mandatory)]
-        [String] $Item,
+        [String] $Name,
         [Parameter(Position = 1, ParameterSetName = "B", Mandatory)]
         [switch] $DefaultItem,
+        # 3. 型態
         [Parameter(Position = 2, ParameterSetName = "A", Mandatory)]
         [ValidateSet(
             'REG_SZ',
@@ -18,11 +21,13 @@ function RegAdd {
             'REG_FULL_RESOURCE_DESCRIPTOR',
             'REG_EXPAND_SZ'
         )] [String] $Type,
+        # 4. 值
         [Parameter(Position = 3, ParameterSetName = "")]
         [String] $Value,
-        
+        # 其他選項
+        [Parameter(ParameterSetName = "")]
         [switch] $OutNull,
-        [switch] $OnlyOutCmd
+        [switch] $RunCmd
     )
     # 修正 Value
     if ($Value.Length -gt 0) { $Value = " /d `"$Value`"" }
@@ -30,33 +35,27 @@ function RegAdd {
     if ($DefaultItem) {
         $Cmd = "reg add `"$Path`" /ve" + $Value + " /f"
     } else {
-        $Cmd = "reg add `"$Path`" /v `"$Item`" /t $Type" + $Value + " /f"
+        $Cmd = "reg add `"$Path`" /v `"$Name`" /t $Type" + $Value + " /f"
     }
     
     # 執行命令
-    if ($OnlyOutCmd) {
-        Write-Host $Cmd
+    if (!$RunCmd) {
+        return $Cmd
     } else {
         if ($OutNull) { $Cmd = $Cmd + '|Out-Null' }
         Invoke-Expression $Cmd
     }
 }
-# $regPath  = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device"
-# $regItem  = "DevicePasswordLessBuildVersion"
-# $regType  = "REG_DWORD"
-# $regValue = 0
-# RegAdd $regPath $regItem $regType $regValue -OnlyOutCmd
+# RegAdd "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" EditionID REG_SZ Core
+# reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "EditionID" /t REG_SZ /d "Core" /f
 
-# $regPath  = "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32"
-# $regValue = ''
-# RegAdd $regPath -DefaultItem $regValue -OnlyOutCmd
 
 function RegDel {
     param (
         [Parameter(Position = 0, ParameterSetName = "", Mandatory)]
         [String] $Path,
         [Parameter(Position = 1, ParameterSetName = "A", Mandatory)]
-        [String] $Item,
+        [String] $Name,
         [Parameter(Position = 1, ParameterSetName = "B", Mandatory)]
         [switch] $DefaultItem,
         [switch] $OutNull,
@@ -66,14 +65,14 @@ function RegDel {
     if ($DefaultItem) {
         $Cmd = "reg delete `"$Path`" /ve /f"
     } else {
-        $Cmd = "reg delete `"$Path`" /v `"$Item`" /f"
+        $Cmd = "reg delete `"$Path`" /v `"$Name`" /f"
     }
     # 執行命令
     if ($OnlyOutCmd) {
         Write-Host $Cmd
     } else {
         if ($OutNull) { $Cmd = $Cmd + '|Out-Null' }
-        if (Get-ItemProperty $('Registry::'+$Path) -Name:$Item -EA:0) {
+        if (Get-ItemProperty $('Registry::'+$Path) -Name:$Name -EA:0) {
             Invoke-Expression $Cmd
         }
     }
