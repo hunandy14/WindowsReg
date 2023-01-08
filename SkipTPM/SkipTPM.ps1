274699eaa8dcfac49e7c808a492af14c0be3f443
Charlotte.Hong <hunandy14@gmail.com>
2023-01-08 22:44:56 +0900

    修正沒有資料夾時的報錯

--- SkipTPM/SkipTPM.ps1
+++ SkipTPM/SkipTPM.ps1
@@ -2,7 +2,9 @@
 $URL = "https://raw.githubusercontent.com/AveYo/MediaCreationTool.bat/main/bypass11/Skip_TPM_Check_on_Dynamic_Update.cmd"
 # 下載
 $URL -match "[^/]+(?!.*/)" |Out-Null
-$Path = $env:temp + "\bypass11\" + $Matches[0]
+$Path = $env:temp + $Matches[0]
 Start-BitsTransfer $URL $Path
 # 執行程序
-explorer.exe $Path
+if (Test-Path $Path) {
+    explorer.exe $Path
+} else { Write-Error "Error:: 檔案下載失敗" }
