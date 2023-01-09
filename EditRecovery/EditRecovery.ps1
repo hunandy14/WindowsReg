6d01e784edaf83ec49c40e5a54d6172d518522af
Charlotte.Hong <hunandy14@gmail.com>
2023-01-09 18:42:28 +0900

    修正容量顯示錯誤的bug 以及升級部分函式

--- EditRecovery/EditRecovery.ps1
+++ EditRecovery/EditRecovery.ps1
@@ -1,38 +1,63 @@
 
 # 獲取RE分區
-function GetRecoveryPartition {
+function Get-RecoveryPartition {
     [string] $RecoveryPath = ((reagentc /info) -match("\\\\\?\\GLOBALROOT\\device"))
     if ($RecoveryPath -eq '') {
         Write-Host "RE分區尚未啟用" -ForegroundColor:Yellow
-        return
+        return $false
     }
     $DiskNum = (([regex]('\\harddisk([0-9]+)\\')).Matches($RecoveryPath)).Groups[1].Value
     $PartNum = (([regex]('\\partition([0-9]+)\\')).Matches($RecoveryPath)).Groups[1].Value
-    return @($DiskNum, $PartNum)
-} # GetRecoveryPartition
+    $Part    = Get-Partition -DiskNumber $DiskNum -PartitionNumber $PartNum
+
+    if (!$Part) { Write-Error "錯誤:: 找不到RE修復分區" -EA:Stop }
+    if ($Part.DriveLetter -eq "C") { Write-Warning "RE分區已經刪除不需要再次執行"; Exit 1 }
+    
+    return [PSCustomObject]@{
+        DiskNumber        = $DiskNum
+        PartitionNumber   = $PartNum
+        RecoveryPartition = $Part
+    }
+} # Get-RecoveryPartition
 
 # 格式化容量單位
 function FormatCapacity {
     param (
-        [Parameter(Position = 0, ParameterSetName = "", Mandatory=$false)]
-        [double] $Capacity=0,
-        [Parameter(Position = 1, ParameterSetName = "", Mandatory=$false)]
-        [double] $Digit=1
+        [Parameter(Position = 0, ParameterSetName = "")]
+        [double] $Value = 0,
+        [Parameter(Position = 1, ParameterSetName = "")]
+        [double] $Digit = 1,
+        [switch] $KB,
+
+        [Parameter(ParameterSetName = "")]
+        [uint64] $Width = 5,
+        [switch] $Align
     )
-    $Unit = 'Byte'
-    if (([Math]::Floor($Capacity)|Measure-Object -Character).Characters -gt 3) {
-        $Capacity = $Capacity/1024.0; $Unit = 'KB'
-    } if (([Math]::Floor($Capacity)|Measure-Object -Character).Characters -gt 3) {
-        $Capacity = $Capacity/1024.0; $Unit = 'MB'
-    } if (([Math]::Floor($Capacity)|Measure-Object -Character).Characters -gt 3) {
-        $Capacity = $Capacity/1024.0; $Unit = 'GB'
-    } if (([Math]::Floor($Capacity)|Measure-Object -Character).Characters -gt 3) {
-        $Capacity = $Capacity/1024.0; $Unit = 'TB'
-    } if (([Math]::Floor($Capacity)|Measure-Object -Character).Characters -gt 3) {
-        $Capacity = $Capacity/1024.0; $Unit = 'PB'
-    } $Capacity = [Math]::Round($Capacity, $Digit)
-    return "$Capacity $Unit"
-} # FormatCapacity
+    # 設定單位
+    [string] $Unit_Type = 'Byte'
+    [double] $Unit = 1024.0
+    $UnitList = @('KB', 'MB', 'GB', 'TB', 'PB')
+   
+    # 開始換算
+    foreach ($Item in $UnitList) {
+        $bool = (([Math]::Floor($Value)|Measure-Object -Character).Characters -gt 3)
+        if ($bool -or $KB) {
+            $Value = $Value/$Unit; $Unit_Type = $Item
+            if ($KB) { break }
+        } else { break }
+    }; $Value = [Math]::Round($Value, $Digit)
+
+    # 對齊自動補上空白
+    if ($Align) {
+        $Space = ""
+        $ValueWidth = $Value.ToString().Length
+        $SpaceCount = $Width - $ValueWidth
+        if ($SpaceCount -gt 0) { (1..$SpaceCount)|ForEach-Object{ $Space += " " } }
+    }
+    return ($Space + "$Value $Unit_Type")
+} # FormatCapacity 18915618941
+
+
 
 # RE系統相關設定
 function EditRecovery {
@@ -62,16 +87,16 @@ function EditRecovery {
         }
         # 刪除RE分區
         if ($Remove) {
+            $RecObj = Get-RecoveryPartition
             # 獲取RE分區位置
-            $Posi = (GetRecoveryPartition)
-            $DN = $Posi[0]
-            $PN = $Posi[1]
-            if (!$Posi) { return }
-            $Part = (Get-Partition -Number:$PN)[$DN]
-            $Capacity = FormatCapacity($Part.Size)
+            $DiskNum = $RecObj.DiskNumber
+            $PartNum = $RecObj.PartitionNumber
+            $Part    = $RecObj.RecoveryPartition
+            $Capacity = FormatCapacity ($Part.Size)
             # 刪除RE分區(警告)
+            ($Part|Get-Disk|Get-Partition) |Format-Table PartitionNumber,@{Name='DriveLetter'; Expression={if($_.DriveLetter){$_.DriveLetter}else{" "}}; Align='right'},@{Name='Size    '; Expression={$(FormatCapacity $_.Size -Align)}; Align='right'},Type -AutoSize
             Write-Host "即將刪除 [" -NoNewline
-            Write-Host "磁碟:$($DN), 分區:$($PN), 容量:$Capacity" -ForegroundColor:Yellow -NoNewline
+            Write-Host "磁碟:$($DiskNum), 分區:$($PartNum), 容量:$Capacity" -ForegroundColor:Yellow -NoNewline
             Write-Host "] 的RE分區, " -NoNewline
             Write-Host "刪除後無法復原" -ForegroundColor:Red -NoNewline
             Write-Host "請確保分區位置是正確的"
@@ -84,7 +109,7 @@ function EditRecovery {
             # 移除RE分區
             $Part|Remove-Partition
             # 合併釋放的空間到前一個分區
-            $PartPre = (Get-Partition -Number:($PN-1))[$DN]
+            $PartPre = Get-Partition -DiskNumber $DiskNum -PartitionNumber ($PartNum-1)
             $PartPre|Resize-Partition -Size:($PartPre|Get-PartitionSupportedSize).SizeMax
         }
         # 重啟RE分區
