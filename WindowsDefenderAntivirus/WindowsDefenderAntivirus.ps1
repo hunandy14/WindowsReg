6de4e78d6b81c65dd684888eb97ec5bbaf9dce8c
Charlotte.Hong <hunandy14@gmail.com>
2023-03-04 14:13:52 +0900

    WindowsDefenderAntivirus:: 關閉選項中追加關閉掃描

--- WindowsDefenderAntivirus/WindowsDefenderAntivirus.ps1
+++ WindowsDefenderAntivirus/WindowsDefenderAntivirus.ps1
@@ -8,14 +8,14 @@ function WindowsDefenderAntivirus {
         [switch] $NotOpenSetting
     )
     if ($Disable) { # 關閉所有功能
-        WindowsDefenderAntivirus
+        reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /f /v DisableRealtimeMonitoring /t REG_DWORD /d 1
         reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /f /v DisableAntiSpyware /t REG_DWORD /d 1
         Write-Host "已關閉防毒軟體，但新版 Windows 需要手動關閉防竄改保護才會生效。" -ForegroundColor:Yellow
         Write-Host "  [右下角系統圖示::Windows安全性 -> 病毒與威脅防護 "
         Write-Host "     -> 病毒與威脅防護設定::管理設定 -> 防竄改保護::關閉此項目]"
         Write-Host "圖文說明可以參考這篇 https://bit.ly/3sAmHhC"
     } elseif ($DisableRealtime) { # 僅關閉即時掃描
-        WindowsDefenderAntivirus
+        WindowsDefenderAntivirus -NotOpenSetting
         reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /f /v DisableRealtimeMonitoring /t REG_DWORD /d 1
         Write-Host "已關閉即時掃描，但新版 Windows 需要手動關閉防竄改保護才會生效。" -ForegroundColor:Yellow
         Write-Host "  [右下角系統圖示::Windows安全性 -> 病毒與威脅防護 "
